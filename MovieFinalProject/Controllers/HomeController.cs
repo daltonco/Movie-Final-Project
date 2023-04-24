fb0fd253394ff5fafd6635cc78f15ae364c60abc
finchea <66384814+finchea@users.noreply.github.com>
2023-04-24 11:32:27 -0400

    Configure EthanAbout view

--- MovieFinalProject/Controllers/HomeController.cs
+++ MovieFinalProject/Controllers/HomeController.cs
@@ -22,6 +22,11 @@ namespace MovieFinalProject.Controllers
         {
             return View();
         }
+        
+        public IActionResult AboutEthan()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
